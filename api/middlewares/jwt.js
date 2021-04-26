e62c3d7c6fdf32dc62362b00d13db46996dec80e
b3457m0d3 <perchance2dream86@gmail.com>
2021-04-25 22:37:22 -0400

    rethink api strategem

--- api/middlewares/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("express-jwt");
-require('dotenv').config()
-const secret = process.env.JWT_SECRET;
-
-const authenticate = jwt({
-	secret: secret
-});
-
-module.exports = authenticate;
